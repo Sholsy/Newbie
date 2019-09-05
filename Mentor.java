a30bbb96782a9b74848a38ea05f7f4e48fbed1ae
unknown <adeshols@yahoo.com>
2019-09-05 15:18:33 +0100

    updated version

---
+++ Mentor.java
@@ -0,0 +1,9 @@
+import java.scanner
+
+public class Mentor
+
+public static void main(String[] args)
+
+System.out.println("hello world")
+System.out.println("hello world")
+System.out.println("hello world")
